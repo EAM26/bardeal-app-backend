c3d649b6f7531d872cd65b82d339bb0f3ecf9f2f
EAM26 <emuller2607@gmail.com>
2025-03-19 14:56:49 +0100

    Setup Project

---
+++ src/main/java/org/eamcod/BardealApp/BardealAppApplication.java
@@ -0,0 +1,13 @@
+package org.eamcod.BardealApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BardealAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BardealAppApplication.class, args);
+	}
+
+}
